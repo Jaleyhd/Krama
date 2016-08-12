b167a97e67ffc6fd6082163802ab8a2079464693
Jaleyhd <jaley.dholakiya@gmail.com>
2016-08-12 23:48:49 +0530

    install configurations updated

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get install python-dev libprotobuf-dev protobuf-compiler python-sphinx python-pip 
 sudo pip install --upgrade pip
-sudo pip install sphinx_rtd_theme unittest2 nose2 tox subprocess py-dag
+sudo pip install sphinx_rtd_theme unittest2 nose2 tox subprocess py-dag sphinxcontrib-napoleon
+sudo apt-get install nodejs npm
 
